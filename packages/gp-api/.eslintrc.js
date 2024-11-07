6733b3acde9633eee000a7405e9dbdfe46d9f2d0
Matthew Marcus <lurchbulldog@gmail.com>
2024-11-07 17:16:42 -0600

    [WEB-3320] - Content Module Scaffolding

--- packages/gp-api/.eslintrc.js
+++ packages/gp-api/.eslintrc.js
@@ -17,9 +17,10 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    semi: 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-};
+}
