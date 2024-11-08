9e894ebdba9bdf67b032d23c3bad40f5bf2432b6
Matthew Marcus <lurchbulldog@gmail.com>
2024-11-08 13:36:48 -0600

    [WEB-3343] - health check endpoint

--- packages/gp-api/src/app.module.ts
+++ packages/gp-api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ContentModule } from './content/content.module'
+import { HealthModule } from './health/health.module'
 
 @Module({
-  imports: [ContentModule],
+  imports: [ContentModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/gp-api/src/health/health.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, Logger } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+@Injectable()
+export class HealthService {
+  private readonly logger = new Logger(HealthService.name)
+  async checkHealth(): Promise<boolean> {
+    // Right now, this just simply checks if the database connection is working,
+    //   but we can add more checks here for other backend services as well
+    try {
+      await prisma.$queryRaw`SELECT 1`
+      return true
+    } catch (e: unknown) {
+      this.logger.error(
+        'Health check failed => ',
+        e instanceof Error ? e.message : e,
+      )
+      return false
+    }
+  }
+}
