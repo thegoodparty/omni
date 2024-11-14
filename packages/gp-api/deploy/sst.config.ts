fc31318bcdff3153c001b2ec6190a80ecf6d1e1c
Taylor Murray <taylor@goodparty.org>
2024-11-14 10:42:04 -0800

    feat: update deployment to use nat gateway and multi az.

--- packages/gp-api/deploy/sst.config.ts
+++ packages/gp-api/deploy/sst.config.ts
@@ -9,12 +9,16 @@ export default $config({
     }
   },
   async run() {
-    const vpc = new sst.aws.Vpc('NestVpc', { bastion: false })
+    const vpc = new sst.aws.Vpc('NestVpc', {
+      bastion: false,
+      nat: 'managed',
+      az: 2, // defaults to 2 availability zones and 2 NAT gateways
+    })
     const cluster = new sst.aws.Cluster('NestCluster', { vpc })
 
     cluster.addService('NestService', {
       loadBalancer: {
-        domain: 'nest.goodparty.org',
+        domain: 'gp-api.goodparty.org',
         ports: [
           { listen: '80/http' },
           { listen: '443/https', forward: '80/http' },
@@ -28,6 +32,13 @@ export default $config({
       // ssm: {
       //   // Key-value pairs of AWS Systems Manager Parameter Store parameter ARNs or AWS Secrets
       //   //  * Manager secret ARNs. The values will be loaded into the container as environment variables.
+      // },
+      // todo: configure health checks.
+      // health: {
+      //   '443/https': {
+      //     path: '/health',
+      //     interval: '10 seconds',
+      //   },
       // },
       image: {
         context: '../../gp-api', // Set the context to the main app directory
