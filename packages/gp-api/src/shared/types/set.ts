5c3ddc89d82ee7a8c99512a2bf8bb84dd1718a06
Matthew Marcus <lurchbulldog@gmail.com>
2024-11-14 14:04:09 -0600

    [WEB-3321] - figured out we need esnext for TS support

--- packages/gp-api/src/shared/types/set.ts
+++
@@ -1,7 +0,0 @@
-// While Set.prototype.intersection and Set.prototype.difference are
-//  indeed supported by Node 22+, apparently TS hasn't updated its Set
-//  type definition to include these methods yet. So we need to do this for now.
-interface Set<T> {
-  intersection(other: Set<T>): Set<T>
-  difference(other: Set<T>): Set<T>
-}
