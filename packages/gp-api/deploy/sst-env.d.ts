ca9a6cd7131d2fca2df9966ec13b6aa67abe9852
Taylor Murray <taylor@goodparty.org>
2024-11-14 12:54:12 -0800

    feat: deploy gp-api.

--- packages/gp-api/deploy/sst-env.d.ts
+++ packages/gp-api/deploy/sst-env.d.ts
@@ -2,8 +2,17 @@
 /* tslint:disable */
 /* eslint-disable */
 /* deno-fmt-ignore-file */
-import 'sst'
+import "sst"
 export {}
-declare module 'sst' {
-  export interface Resource {}
+declare module "sst" {
+  export interface Resource {
+    "GP-Service": {
+      "service": string
+      "type": "sst.aws.Service"
+      "url": string
+    }
+    "GP-VPC": {
+      "type": "sst.aws.Vpc"
+    }
+  }
 }
