bacfe5ab741d3e062938d0b6fb4036e14dc165a4
oakinh <nikaohill@gmail.com>
2024-11-26 09:50:26 -0600

    Make improvements to declare service

--- packages/gp-api/api/src/declare/declare.service.ts
+++ packages/gp-api/api/src/declare/declare.service.ts
@@ -1,89 +1,58 @@
-import { Injectable, HttpException, HttpStatus, Logger, BadGatewayException } from '@nestjs/common';
-
-interface Submission {
-  values: { value: string}[];
-}
-
-interface HubspotResponse {
-  results: Submission[];
-}
-
-function isHubspotResponse(data: any): data is HubspotResponse {
-  return (
-    data &&
-    Array.isArray(data.results) &&
-    data.results.every(
-      (submission: any) =>
-        Array.isArray(submission.values) &&
-        submission.values.every(
-          (value: any) => typeof value.value === 'string',
-        ),
-    )
-  );
-}
+import { Injectable, Logger, BadGatewayException } from '@nestjs/common';
+import { Client } from '@hubspot/api-client';
 
 @Injectable()
 export class DeclareService {
   private readonly logger = new Logger(DeclareService.name);
-  private readonly hubspotToken = process.env.HUBSPOT_TOKEN;
+  private readonly hubspotClient: Client;
 
+  constructor () {
+    const hubspotToken = process.env.HUBSPOT_TOKEN;
+    if (!hubspotToken) {
+      this.logger.error('HUBSPOT_TOKEN is not defined in env variables');
+      throw new Error('HUBSPOT_TOKEN is required');
+    }
+    this.hubspotClient = new Client({accessToken: hubspotToken });
+  }
   async getDeclarations(): Promise<{ signatures: string }> {
     const formId = 'f51c1352-c778-40a8-b589-b911c31e64b1';
-    const url = `https://api.hubapi.com/form-integrations/v1/submissions/forms/${formId}`;
 
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${this.hubspotToken}`,
-      },
-    });
-
-    if (!response.ok) {
-      this.logger.error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+    let response;
+    try {
+      response = await this.hubspotClient.marketing.forms.formsApi.getById(formId);
+    } catch (error: any) {
+      this.logger.error(
+        `Failed to fetch data from HubSpot API: ${error.message}`,
+        error?.response?.data || error.stack,
+      );
       throw new BadGatewayException('Failed to fetch data from Hubspot API');
     }
 
-    const responseData = await response.json();
+    const data = response.results;
 
-    if (!isHubspotResponse(responseData)) {
-      this.logger.error('Invalid response structure from Hubspot API', {
-        response: responseData,
-      });
-      throw new BadGatewayException('Invalid reponse structure from Hubspot API');
-    }
+    const uniqueSignatures = new Set<string>();
 
-    const data = responseData.results;
-
-    const signaturesObj: Record<string, boolean> = {};
-    let signatures = '';
-
-    if (data && data.length > 0) {
+    if (data?.length) {
       for (const submission of data) {
         if (submission.values.length > 0) {
-          let fn = submission.values[0].value;
-          let ln = submission.values[1].value;
+          let firstName = submission.values[0].value;
+          let lastName = submission.values[1].value;
           // format the names to look nice and prevent duplicates.
-          if (fn && fn.length >= 2) {
-            fn =
-              fn.charAt(0).toUpperCase() + fn.slice(1).toLowerCase().trim();
+          if (firstName && firstName.length >= 2) {
+            firstName =
+              firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase().trim();
           }
-          if (ln && ln.length >= 2) {
-            ln =
-              ln.charAt(0).toUpperCase() + ln.slice(1).toLowerCase().trim();
+          if (lastName && lastName.length >= 2) {
+            lastName =
+              lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase().trim();
           }
 
-          const name = `${fn} ${ln}`;
-          if (!signaturesObj[name]) {
-            signatures += `${name}, `;
-            signaturesObj[name] = true;
-          }
+          uniqueSignatures.add(`${firstName} ${lastName}`)
         }
       }
     }
-    
-    if (signatures.length > 2) {
-      signatures = signatures.slice(0, -2);
-    }
+
+    const signatures = Array.from(uniqueSignatures).join(', ');
 
     return { signatures };
   }
