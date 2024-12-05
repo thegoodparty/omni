a331b14e7f96d1414a4d8a8155eab65cd8fc005b
Matthew Marcus <lurchbulldog@gmail.com>
2024-12-05 10:55:06 -0600

    [WEB-3398] - Prisma 6 upgrade

--- packages/gp-api/src/authentication/authentication.module.ts
+++ packages/gp-api/src/authentication/authentication.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
 import { AuthenticationService } from './authentication.service'
 import { UsersModule } from '../users/users.module'
 import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
-import { AuthenticationController } from './authentication.controller';
+import { AuthenticationController } from './authentication.controller'
 
 const JWT_EXPIRATION = '1y'
+
+if (!process.env.AUTH_SECRET) {
+  const logger = new Logger('AuthenticationModule')
+  const msg = 'AUTH_SECRET is required for application startup'
+  logger.error(msg)
+  throw new Error(msg)
+}
 
 @Module({
   providers: [AuthenticationService],
