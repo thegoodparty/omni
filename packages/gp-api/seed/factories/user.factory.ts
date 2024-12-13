88673f8c769ae7ac994cf1e99d00ba5b61d6a6f2
Sanjeev Nayak <snayak00@gmail.com>
2024-12-13 13:38:07 -0500

    WEB-3237: remove admin from roles for seeded users

--- packages/gp-api/seed/factories/user.factory.ts
+++ packages/gp-api/seed/factories/user.factory.ts
@@ -3,6 +3,9 @@ import { faker } from '@faker-js/faker'
 import { generateFactory } from './generate'
 import { generateRandomPassword } from '../../src/users/util/passwords.util'
 
+// filter admin from seeded users
+const seedRoles = Object.values(UserRole).filter((r) => r !== UserRole.admin)
+
 export const userFactory = generateFactory<User>(() => {
   const firstName = faker.person.firstName()
   const lastName = faker.person.lastName()
@@ -18,7 +21,7 @@ export const userFactory = generateFactory<User>(() => {
     password: generateRandomPassword(),
     phone: faker.string.numeric({ length: 10, allowLeadingZeros: false }),
     zip: faker.location.zipCode(),
-    roles: [faker.helpers.enumValue(UserRole)],
+    roles: [faker.helpers.arrayElement(seedRoles)],
     metaData: {},
   }
 })
