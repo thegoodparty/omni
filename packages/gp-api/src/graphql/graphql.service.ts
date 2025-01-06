f8a1ddbe5c880fd154410a6c7a39f04c5f2ae0b1
Sanjeev Nayak <snayak00@gmail.com>
2025-01-06 16:53:34 -0500

    fix: move instantiation of GQL client to provider

--- packages/gp-api/src/graphql/graphql.service.ts
+++ packages/gp-api/src/graphql/graphql.service.ts
@@ -2,22 +2,10 @@ import { Injectable } from '@nestjs/common'
 import { GraphQLClient, gql } from 'graphql-request'
 import { Logger } from '@nestjs/common'
 
-// note: if we ever need to support multiple api bases
-// we can cross that bridge when we get to it
-const API_BASE = 'https://bpi.civicengine.com/graphql'
-const BALLOT_READY_KEY = process.env.BALLOT_READY_KEY
-
 @Injectable()
 export class GraphqlService {
   private readonly logger = new Logger(GraphqlService.name)
-  constructor(private readonly graphQLClient: GraphQLClient) {
-    graphQLClient = new GraphQLClient(API_BASE, {
-      headers: {
-        authorization: `Bearer ${BALLOT_READY_KEY}`,
-        'Content-Type': 'application/json',
-      },
-    })
-  }
+  constructor(private readonly graphQLClient: GraphQLClient) {}
 
   async fetchGraphql(query: any, variables?: any) {
     let data: any
