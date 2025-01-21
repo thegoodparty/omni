711ca89ee4732d452ec381b4c4c1cfc3e58a593a
Matthew Marcus <lurchbulldog@gmail.com>
2025-01-21 10:35:58 -0600

    Update UpdateMetadata.schema.ts
    Added TODO

--- packages/gp-api/src/users/schemas/UpdateMetadata.schema.ts
+++ packages/gp-api/src/users/schemas/UpdateMetadata.schema.ts
@@ -2,6 +2,8 @@ import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
 
 export class UpdateMetadataSchema extends createZodDto(
+  // TODO: Once the UserMetaData TS type is fleshed out, we need
+  //  to explicitly type this validation.
   z.object({
     meta: z.record(
       z.string(),
