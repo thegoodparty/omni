2b253a7074b6a6ad00e9e7f27123f968d73e3346
Sanjeev Nayak <snayak00@gmail.com>
2025-01-29 13:30:29 -0500

    remove unused file

--- packages/gp-api/src/prisma/prisma.types.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-
-export type PrismaModelNames = Prisma.TypeMap['meta']['modelProps']
-
-export type PrismaModelMethods = {
-  [M in PrismaModelNames]: keyof Prisma.TypeMap['model'][Capitalize<M>]['operations']
-}[PrismaModelNames]
-
-export type PrismaClientMethods = Extract<keyof PrismaClient, `$${string}`>
