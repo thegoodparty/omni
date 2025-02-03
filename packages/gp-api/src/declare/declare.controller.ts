d4a4baf23b1a134602042d1e64c16e16d95eb228
Sanjeev Nayak <snayak00@gmail.com>
2025-02-03 15:10:08 -0500

    WEB-3582: fix declare endpoint not returning json response

--- packages/gp-api/src/declare/declare.controller.ts
+++ packages/gp-api/src/declare/declare.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common'
 import { DeclareService } from './declare.service'
+import { PublicAccess } from 'src/authentication/decorators/PublicAccess.decorator'
 
 @Controller('declare')
 export class DeclareController {
   constructor(private readonly declareService: DeclareService) {}
 
   @Get('list')
+  @PublicAccess()
   async listDeclarations() {
     return await this.declareService.getDeclarations()
   }
