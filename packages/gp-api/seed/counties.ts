a3565c6ccaf1a0c0643fa6389dac69238661eae6
Sanjeev Nayak <snayak00@gmail.com>
2025-02-13 11:42:44 -0500

    WEB-3441: added mtfcc, election type, county, and municipality seeds, and csv files

---
+++ packages/gp-api/seed/counties.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from '@prisma/client'
+import slugify from 'slugify'
+import path from 'path'
+import { loadCSV } from './util/csv.util'
+
+type ExpectedCSVRow = {
+  county: string
+  state_id: string
+}
+
+let count = 0
+export default async function seedCounties(
+  prisma: PrismaClient,
+  loadAll = false,
+) {
+  const csvFilePath = loadAll // use full dataset for prod/qa, short dataset for dev
+    ? path.join(
+        __dirname,
+        './data/geoPoliticalEntities/dec23/uscounties_v1.73.csv',
+      )
+    : path.join(
+        __dirname,
+        './data/geoPoliticalEntities/dec23/uscounties_v1.73_short.csv',
+      )
+
+  console.log('Reading Counties from csv')
+
+  const rows = await loadCSV<ExpectedCSVRow>(csvFilePath)
+
+  const total = rows.length
+  for (let i = 0; i < total; i++) {
+    const percentComplete = (((i + 1) / total) * 100).toFixed(0)
+    process.stdout.write(`\r ${i + 1}/${total} ${percentComplete}% complete`)
+
+    const row = rows[i]
+    const { county, state_id } = row
+    const slug = `${slugify(state_id, { lower: true })}/${slugify(county, {
+      lower: true,
+    })}`
+
+    await prisma.county.upsert({
+      where: {
+        slug,
+      },
+      update: {},
+      create: {
+        slug,
+        name: county,
+        state: state_id,
+        data: row,
+      },
+    })
+
+    count++
+  }
+  console.log(`\ninserted ${count} counties`)
+}
--- packages/gp-api/seed/seed.ts
+++ packages/gp-api/seed/seed.ts
@@ -1,16 +1,50 @@
 import { PrismaClient } from '@prisma/client'
-import seedRaces from './races.seed'
+import { parseArgs } from 'util'
+
+// factory seeds
+import seedRaces from './races'
 import seedCampaigns from './campaigns'
 import seedTopIssues from './topIssues'
 import seedUsers from './users'
 
+// csv file seeds
+import seedElectionTypes from './electionTypes'
+import seedMtfcc from './mtfcc'
+import seedCounties from './counties'
+import seedMunicipalities from './municipalities'
+
+const IS_PROD_BUILD =
+  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'qa'
+
 const prisma = new PrismaClient()
 
 async function main() {
-  await seedRaces(prisma)
-  const users = await seedUsers(prisma)
-  const campaignIds = await seedCampaigns(prisma, users)
-  await seedTopIssues(prisma, campaignIds)
+  if (IS_PROD_BUILD) {
+    // only want to run seeds from CSV files in prod build
+    await csvSeeds(prisma, true)
+  } else {
+    const seedType = getTypeArg()
+
+    if (seedType === 'csv' || seedType === 'all') {
+      await csvSeeds(prisma)
+
+      // if only running csv file seeds, return early
+      if (seedType === 'csv') return
+    }
+
+    // run factory seeds
+    await seedRaces(prisma)
+    const users = await seedUsers(prisma)
+    const campaignIds = await seedCampaigns(prisma, users)
+    await seedTopIssues(prisma, campaignIds)
+  }
+}
+
+async function csvSeeds(prisma: PrismaClient, loadAll = false) {
+  await seedMtfcc(prisma)
+  await seedElectionTypes(prisma)
+  await seedCounties(prisma, loadAll)
+  await seedMunicipalities(prisma, loadAll)
 }
 
 main()
@@ -22,3 +56,12 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+
+function getTypeArg() {
+  const { values } = parseArgs({
+    options: {
+      type: { type: 'string', short: 't' },
+    },
+  })
+  return values.type
+}
