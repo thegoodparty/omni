d2c656c8de70dafdad71d3fdc758bf347aed1e97
oakinh <nikaohill@gmail.com>
2025-03-12 14:56:51 -0500

    Add year calc to racesByState

--- packages/election-api/src/races/races.controller.ts
+++ packages/election-api/src/races/races.controller.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common'
 import { RacesService } from './races.service'
+import { PositionLevel } from '@prisma/client'
 
 @Controller('races')
 export class RacesController {
@@ -10,6 +11,21 @@ export class RacesController {
     if (state.trim().length !== 2) {
       throw new BadRequestException('State must be a 2 letter abbreviation')
     }
-    this.racesService.findMany({where: { state }})
+    state = state.toUpperCase()
+
+    const now = new Date()
+
+    const startOfTwoYearsFromNow = new Date()
+    startOfTwoYearsFromNow.setFullYear(startOfTwoYearsFromNow.getFullYear() + 2)
+    startOfTwoYearsFromNow.setMonth(0) // Jan
+    startOfTwoYearsFromNow.setDate(1) // First day
+
+
+    const races = this.racesService.findMany({
+      where: { 
+        state,
+        positionLevel: PositionLevel.STATE,
+
+      }})
   }
 }
