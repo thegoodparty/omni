3081615225ac642cc4aec619cde8b215e32ae7a4
oakinh <nikaohill@gmail.com>
2025-03-13 15:43:35 -0500

    Add more endpoints to the race controller

--- packages/election-api/src/races/races.controller.ts
+++ packages/election-api/src/races/races.controller.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common'
 import { RacesService } from './races.service'
 import { PositionLevel } from '@prisma/client'
+import slugify from 'slugify'
 
 @Controller('races')
 export class RacesController {
@@ -42,5 +43,105 @@ export class RacesController {
       throw new BadRequestException('State must be a 2 letter abbreviation')
     }
     state = state.toUpperCase()
+
+    const startOfTwoYearsFromNow = new Date()
+    startOfTwoYearsFromNow.setFullYear(startOfTwoYearsFromNow.getFullYear() + 2)
+    startOfTwoYearsFromNow.setMonth(0) // Jan
+    startOfTwoYearsFromNow.setDate(1) // First day
+
+    const races = await this.racesService.findMany({
+      where: {
+        state,
+        electionDate: {
+          lt: startOfTwoYearsFromNow,
+          gt: new Date(),
+        },
+      },
+      orderBy: {
+        electionDate: 'asc'
+      },
+      distinct: ['positionSlug']
+    })
+
+    return races
   }
+
+  @Get('by-county')
+  async racesByCounty(@Query('state', 'county') state: string, county: string) {
+    if (state.trim().length !== 2) {
+      throw new BadRequestException('State must be a 2 letter abbreviation')
+    }
+    state = state.toUpperCase()
+
+    const startOfTwoYearsFromNow = new Date()
+    startOfTwoYearsFromNow.setFullYear(startOfTwoYearsFromNow.getFullYear() + 2)
+    startOfTwoYearsFromNow.setMonth(0) // Jan
+    startOfTwoYearsFromNow.setDate(1) // First day
+
+    const countySlug = `${slugify(state, {lower: true})}-${slugify(county), {lower:true}}`
+
+    const races = await this.racesService.findMany({
+      where: {
+        countySlug, 
+        electionDate: {
+          lt: startOfTwoYearsFromNow,
+          gt: new Date(),
+        },
+        positionLevel: PositionLevel.COUNTY
+      },
+      orderBy: {
+        electionDate: 'asc'
+      },
+      distinct: ['positionSlug']
+    })
+
+    return races
+  }
+
+  @Get('by-municipality')
+  async racesByMunicipality(@Query('state', 'county', 'municipality') state: string, county: string, municipality: string) {
+    if (state.trim().length !== 2) {
+      throw new BadRequestException('State must be a 2 letter abbreviation')
+    }
+    state = state.toUpperCase()
+    
+    const municipalitySlug = `${slugify(state, { lower: true })}/${slugify(county, {
+        lower: true,
+      })}/${slugify(municipality, {
+        lower: true,
+      })}`
+
+    const startOfTwoYearsFromNow = new Date()
+    startOfTwoYearsFromNow.setFullYear(startOfTwoYearsFromNow.getFullYear() + 2)
+    startOfTwoYearsFromNow.setMonth(0) // Jan
+    startOfTwoYearsFromNow.setDate(1) // First day
+
+    const races = await this.racesService.findMany({
+      where: {
+        municipalitySlug, 
+        electionDate: {
+          lt: startOfTwoYearsFromNow,
+          gt: new Date(),
+        },
+      },
+      orderBy: {
+        electionDate: 'asc'
+      },
+      distinct: ['positionSlug']
+    })
+
+    const shortCity = {
+      population: races[0].municipalityPopluation,
+      density: races[0].municipalityDensity,
+      incomeHouseholdMedian: races[0].municipalityIncomeHouseholdMedian,
+      unemploymentRate: races[0].municipalityUnemploymentRate,
+      homeValue: races[0].municipalityHomeValue,
+      countyName: races[0].countyName
+    }
+
+    return {
+      races,
+      shortCity
+    }
+  } 
 }
