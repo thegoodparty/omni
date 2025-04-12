dd213caf648687a9f31e0eed03d5234e0ff26832
oakinh <nikaohill@gmail.com>
2025-04-11 19:31:02 -0500

    Add column checking to the zod schema

--- packages/election-api/src/places/places.schema.ts
+++ packages/election-api/src/places/places.schema.ts
@@ -1,6 +1,10 @@
+import { Prisma } from '@prisma/client'
 import { createZodDto } from 'nestjs-zod'
 import { STATE_CODES } from 'src/shared/constants/states'
-import { z } from 'zod'
+import { z, ZodEnum } from 'zod'
+
+const placeColumns = Object.values(Prisma.PlaceScalarFieldEnum) as (keyof typeof Prisma.PlaceScalarFieldEnum)[]
+const raceColumns = Object.values(Prisma.RaceScalarFieldEnum) as (keyof typeof Prisma.RaceScalarFieldEnum)[]
 
 const placeFilterSchema = z.object({
   state: z
@@ -29,8 +33,22 @@ const placeFilterSchema = z.object({
     (val) => (val === undefined ? undefined : parseInt(String(val), 10)),
     z.number().optional().default(1),
   ),
-  placeColumns: z.string().optional(),
-  raceColumns: z.string().optional()
+  placeColumns: z.string().optional().refine(
+    val => {
+      if (!val) return true
+      const columns = val.split(',').map(col => col.trim())
+      return columns.every(col => placeColumns.includes(col as keyof typeof Prisma.PlaceScalarFieldEnum))
+    },
+    { message: `Invalid place column provided. Allowed columns are: ${placeColumns.join(', ')}` }
+  ),
+  raceColumns: z.string().optional().refine(
+    val => {
+      if (!val) return true
+      const columns = val.split(',').map(col => col.trim())
+      return columns.every(col => raceColumns.includes(col as keyof typeof Prisma.RaceScalarFieldEnum))
+    },
+    { message: `Invalid race column provided. Allowed columns are: ${raceColumns.join(', ')}`}
+  )
 })
 
 export class PlaceFilterDto extends createZodDto(placeFilterSchema) {}
