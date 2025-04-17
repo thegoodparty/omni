ad7f62e14af64dec583b3b3ca36bf006dc44df79
oakinh <nikaohill@gmail.com>
2025-04-17 16:39:51 -0500

    Edit SQL in migration files

--- packages/election-api/prisma/migrations/20250417173521_race_slug_index/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "Race_id_slug_idx" ON "Race"("id", "slug");
--- packages/election-api/prisma/migrations/20250417183845_single_index_on_race/migration.sql
+++ packages/election-api/prisma/migrations/20250417183845_single_index_on_race/migration.sql
@@ -1,5 +1,2 @@
--- DropIndex
-DROP INDEX "Race_id_slug_idx";
-
 -- CreateIndex
 CREATE INDEX "Race_slug_idx" ON "Race"("slug");
