a6bc65105d78e6191a32f4c874a440748ec75822
oakinh <nikaohill@gmail.com>
2025-04-16 19:23:25 -0500

    lint

--- packages/election-api/src/places/places.module.ts
+++ packages/election-api/src/places/places.module.ts
@@ -6,6 +6,6 @@ import { PrismaModule } from 'src/prisma/prisma.module'
 @Module({
   controllers: [PlaceController],
   providers: [PlacesService],
-  imports: [PrismaModule]
+  imports: [PrismaModule],
 })
 export class PlacesModule {}
