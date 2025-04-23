e8aadfa14687aaba01b93f2ae4b4c588963c1f4e
Matthew Marcus <lurchbulldog@gmail.com>
2025-04-23 08:39:28 -0500

    [WEB-4079] - Detects when user requests cancelation vs api cancel_at being set (#259)

--- packages/gp-api/src/payments/payments.module.ts
+++ packages/gp-api/src/payments/payments.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { StripeService } from '../stripe/services/stripe.service'
 import { PurchaseController } from './purchase.controller'
 import { PaymentsController } from './payments.controller'
-import { PaymentEventsService } from './stripe/paymentEventsService'
+import { PaymentEventsService } from './services/paymentEventsService'
 import { EmailModule } from '../email/email.module'
 import { CampaignsModule } from '../campaigns/campaigns.module'
 
