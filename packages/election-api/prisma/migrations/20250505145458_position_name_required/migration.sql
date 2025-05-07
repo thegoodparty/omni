08397c12d2e5cf442283f265f09868699000ed9f
tomer-tgp <52380560+tomer-tgp@users.noreply.github.com>
2025-05-07 15:26:34 -0400

    dv (#39)
    * feat: revert CORS origin for dev (#28)
    
    * Web 3732 (#26)
    
    * Make changes to schemas
    
    * More changes to candidacy
    
    * Make changes to candidacy schema
    
    * Draft
    
    * WEB-3732
    
    * Run migration
    
    * Remove p2v json
    
    * Change id type, add createdAt updatedAt columns
    
    * Remove education and military service from candidacy
    
    * Run migration
    
    * edit comment on 'about' for clarity
    
    * Add one to many relation for candidacy and race
    
    * .
    
    * Run migration
    
    * Add brDatabaseId to stance
    
    * Fix column names (#32)
    
    * Fix column names
    
    * More column name fixes
    
    * schema: update Candidacy data types (#33)
    
    * schema: update Candidacy data types
    
    * add migration script
    
    * add line break back in
    
    * Make party nullable on candidacy (#35)
    
    * Make places zod schema non strict temporarily (#36)
    
    * Web 4094 - Categorize Places (#31)
    
    * Add dedupeRacesInTree helper
    
    * Finish logic for categorizing places
    
    * Rename filter field
    
    * Add more filter booleans for optimizing queries
    
    * Rename buckets -> categories
    
    * Switch to using an object for mtfccs o(1) lookup
    
    * Add positionName to candidacy (#37)
    
    * Add positionName to candidacy
    
    * Make positionName required
    
    * Comment out failing migration (#38)
    
    ---------
    
    Co-authored-by: Nikao Hill <145415012+oakinh@users.noreply.github.com>
    Co-authored-by: Hugh Karimi <hussain.h.karimi@gmail.com>

---
+++ packages/election-api/prisma/migrations/20250505145458_position_name_required/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Made the column `position_name` on table `Candidacy` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+-- ALTER TABLE "Candidacy" ALTER COLUMN "position_name" SET NOT NULL;
