eb22764d109d5839bb2e1a48c0f0a62bfb4f0450
Matthew Marcus <lurchbulldog@gmail.com>
2025-07-09 17:48:00 -0500

    [WEB-4334] - Deleted unused controller

--- packages/gp-api/src/peerly/peerly.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common'
-import { PeerlyAuthenticationService } from './services/peerlyAuthenticationService'
-
-@Controller('peerly')
-export class PeerlyController {
-  constructor(private readonly peerlyService: PeerlyAuthenticationService) {}
-}
--- packages/gp-api/src/peerly/peerly.module.ts
+++ packages/gp-api/src/peerly/peerly.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
 import { HttpModule } from '@nestjs/axios'
-import { PeerlyController } from './peerly.controller'
 import { PeerlyAuthenticationService } from './services/peerlyAuthenticationService'
 
 @Module({
   imports: [HttpModule],
-  controllers: [PeerlyController],
   providers: [PeerlyAuthenticationService],
   exports: [PeerlyAuthenticationService],
 })
