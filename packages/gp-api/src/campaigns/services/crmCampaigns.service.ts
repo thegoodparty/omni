cd50f775d56d9a16a6b5e64f0f9231444207e53e
Tomer Almog <tomer@goodparty.org>
2025-08-02 09:29:26 -0400

    chore: add houses count to hubspot

--- packages/gp-api/src/campaigns/services/crmCampaigns.service.ts
+++ packages/gp-api/src/campaigns/services/crmCampaigns.service.ts
@@ -276,12 +276,14 @@ export class CrmCampaignsService {
 
     const ecanvasser = await this.ecanvasser.findByCampaignId(campaignId)
     let ecanvasserCount = 0
+    let ecanvasserHousesCount = 0
     let ecanvasserInteractionsCount = 0
     if (ecanvasser) {
       // get count of contacts and interactions
       const { contacts, interactions } = ecanvasser
       ecanvasserCount = contacts.length
       ecanvasserInteractionsCount = interactions.length
+      ecanvasserHousesCount = ecanvasser.houses.length
     }
 
     const fieldsToSync: Record<
@@ -298,7 +300,7 @@ export class CrmCampaignsService {
       yard_signs_impressions: reportedVoterGoals?.yardSigns,
       // p2p_texts: reportedVoterGoals?.text, TODO: we need a new field in HS for sms text contact numbers!!!
       ecanvasser_contacts_count: ecanvasserCount,
-
+      ecanvasser_houses_count: ecanvasserHousesCount,
       // candidate details
       candidate_district: district,
       candidate_email: user?.email,
