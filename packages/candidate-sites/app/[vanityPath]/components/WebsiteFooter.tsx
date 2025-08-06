3d00117bb68f2095194924ec4a73d7b1e04f41fe
Tomer Almog <tomer@goodparty.org>
2025-08-06 19:45:48 -0400

    fix: visuals

--- packages/candidate-sites/app/[vanityPath]/components/WebsiteFooter.tsx
+++ packages/candidate-sites/app/[vanityPath]/components/WebsiteFooter.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Button from '@/app/shared/buttons/Button'
 import { WebsiteTheme } from '../types/website.type'
 
 interface WebsiteFooterProps {
@@ -18,13 +17,12 @@ export default function WebsiteFooter({
         <p className="text-base mb-4">Paid for and authorized by {committee}</p>
         <p className="text-sm mb-4">
           &copy; {new Date().getFullYear()} • All Rights Reserved •{' '}
-          <Button
-            className="text-sm hover:underline"
-            variant="text"
+          <a
+            className="hover:underline"
             href="?privacy=true"
           >
             Privacy Policy
-          </Button>
+          </a>
         </p>
 
         <div className="flex items-center justify-center gap-2 text-xs text-gray-500 mt-2 pt-2 ">
