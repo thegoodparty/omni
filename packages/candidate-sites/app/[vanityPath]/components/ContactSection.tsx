5e290c5678f559707e703725dc34b681d68ac18e
fromjustin <hanson.justinw@gmail.com>
2025-08-07 12:35:59 -0400

    removed the br

--- packages/candidate-sites/app/[vanityPath]/components/ContactSection.tsx
+++ packages/candidate-sites/app/[vanityPath]/components/ContactSection.tsx
@@ -169,8 +169,7 @@ export default function ContactSection({
                 }}
               />
               <label htmlFor="sms-consent" className="text-xs text-left">
-              By providing your telephone number and checking this box, you consent to receive calls and text messages.<br/>
-              Msg & data rates may apply. Msg frequency may vary. Messaging may include requests for donations.  
+              By providing your telephone number and checking this box, you consent to receive calls and text messages. Msg & data rates may apply. Msg frequency may vary. Messaging may include requests for donations.  
               Reply &quot;STOP&quot; to opt-out &quot;HELP&quot; for help. View{' '}
                 <Link href="?privacy=true" scroll={false}
                   className="text-blue-600 underline hover:text-blue-700"
