07dc972c99e2f86baebadb5dd9229d8e7711146f
Matthew Marcus <lurchbulldog@gmail.com>
2025-09-04 23:02:26 -0500

    [WEB-4601] - Add user email to all tracking calls (#522)
    * [WEB-4601] - Add user email to all tracking calls
    
    * [WEB-4601] - https://github.com/thegoodparty/gp-api/pull/522#discussion_r2324024460
    
    * [WEB-4601] - https://github.com/thegoodparty/gp-api/pull/522#discussion_r2324008813

--- packages/gp-api/src/analytics/analytics.module.ts
+++ packages/gp-api/src/analytics/analytics.module.ts
@@ -1,13 +1,13 @@
-import { Global, Module } from '@nestjs/common'
+import { forwardRef, Global, Module } from '@nestjs/common'
 import { AnalyticsService } from './analytics.service'
 import { HttpModule } from '@nestjs/axios'
 import { SegmentModule } from 'src/segment/segment.module'
-import { SharedModule } from 'src/shared/shared.module'
+import { UsersModule } from '../users/users.module'
 
 @Global()
 @Module({
   providers: [AnalyticsService],
   exports: [AnalyticsService],
-  imports: [HttpModule, SegmentModule, SharedModule],
+  imports: [HttpModule, SegmentModule, forwardRef(() => UsersModule)],
 })
 export class AnalyticsModule {}
