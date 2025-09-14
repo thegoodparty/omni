f5caa4b2ceb88b4b62ddc9b0fa733d44fc999017
Tomer Almog <tomer@goodparty.org>
2025-09-14 19:14:36 -0400

    new relic integration

---
+++ packages/gp-api/newrelic.js
@@ -0,0 +1,37 @@
+'use strict'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Only enable New Relic if both app name and license key are provided
+if (!process.env.NEW_RELIC_APP_NAME || !process.env.NEW_RELIC_LICENSE_KEY) {
+  console.log(
+    'New Relic disabled: Missing NEW_RELIC_APP_NAME or NEW_RELIC_LICENSE_KEY',
+  )
+  module.exports = {}
+} else {
+  console.log(`New Relic enabled for: ${process.env.NEW_RELIC_APP_NAME}`)
+
+  exports.config = {
+    app_name: [process.env.NEW_RELIC_APP_NAME],
+    license_key: process.env.NEW_RELIC_LICENSE_KEY,
+    logging: {
+      level: 'info',
+    },
+    allow_all_headers: true,
+    attributes: {
+      exclude: [
+        'request.headers.cookie',
+        'request.headers.authorization',
+        'request.headers.proxyAuthorization',
+        'request.headers.setCookie*',
+        'request.headers.x*',
+        'response.headers.cookie',
+        'response.headers.authorization',
+        'response.headers.proxyAuthorization',
+        'response.headers.setCookie*',
+        'response.headers.x*',
+      ],
+    },
+  }
+}
