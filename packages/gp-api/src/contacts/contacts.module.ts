86faaf97d0aa7e8ecb4f5d3379a758e0f1cc0139
oakinh <nikaohill@gmail.com>
2025-09-15 14:55:05 -0500

    Clean district names

--- packages/gp-api/src/contacts/contacts.module.ts
+++ packages/gp-api/src/contacts/contacts.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common'
 import { HttpModule } from '@nestjs/axios'
+import { Module } from '@nestjs/common'
 import { CampaignsModule } from 'src/campaigns/campaigns.module'
+import { ElectionsModule } from 'src/elections/elections.module'
 import { ContactsController } from './contacts.controller'
-import { ContactsService } from './services/contacts.service'
 import { ContactsSegmentModule } from './contactsSegment/contactsSegment.module'
+import { ContactsService } from './services/contacts.service'
 
 @Module({
-  imports: [HttpModule, CampaignsModule, ContactsSegmentModule],
+  imports: [
+    HttpModule,
+    CampaignsModule,
+    ContactsSegmentModule,
+    ElectionsModule,
+  ],
   controllers: [ContactsController],
   providers: [ContactsService],
   exports: [ContactsService],
