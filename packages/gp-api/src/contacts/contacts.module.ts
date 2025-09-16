e2ec105c5935e5cb873b34b74a9f4f6cea6fec97
Tomer Almog <tomer@goodparty.org>
2025-09-16 19:03:52 -0400

    chore: delete contactSegment

--- packages/gp-api/src/contacts/contacts.module.ts
+++ packages/gp-api/src/contacts/contacts.module.ts
@@ -2,17 +2,12 @@ import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
 import { CampaignsModule } from 'src/campaigns/campaigns.module'
 import { ElectionsModule } from 'src/elections/elections.module'
+import { VotersModule } from 'src/voters/voters.module'
 import { ContactsController } from './contacts.controller'
-import { ContactsSegmentModule } from './contactsSegment/contactsSegment.module'
 import { ContactsService } from './services/contacts.service'
 
 @Module({
-  imports: [
-    HttpModule,
-    CampaignsModule,
-    ContactsSegmentModule,
-    ElectionsModule,
-  ],
+  imports: [HttpModule, CampaignsModule, VotersModule, ElectionsModule],
   controllers: [ContactsController],
   providers: [ContactsService],
   exports: [ContactsService],
