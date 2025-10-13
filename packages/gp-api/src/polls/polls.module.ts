8293df56527c17f0fa9e42c99caf625438dd7242
Swain Molster <swain.molster@gmail.com>
2025-10-13 09:14:53 -0500

    ENG-4407: track analytics event on poll results complete (#636)

--- packages/gp-api/src/polls/polls.module.ts
+++ packages/gp-api/src/polls/polls.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { PollsController } from './polls.controller'
 import { PollsService } from './services/polls.service'
+import { AnalyticsModule } from 'src/analytics/analytics.module'
 
 @Module({
-  imports: [],
+  imports: [AnalyticsModule],
   providers: [PollsService],
   controllers: [PollsController],
   exports: [PollsService],
