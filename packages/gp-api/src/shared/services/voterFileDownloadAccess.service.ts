aff655b968d5606f4c053f7d2219ae0a95817e74
awardquick <aaron@goodparty.org>
2026-01-13 14:52:35 -0500

    small logic update to more directly handle the can download

--- packages/gp-api/src/shared/services/voterFileDownloadAccess.service.ts
+++ packages/gp-api/src/shared/services/voterFileDownloadAccess.service.ts
@@ -1,46 +1,47 @@
-import { CampaignWith } from '../../campaigns/campaigns.types'
 import { Logger } from '@nestjs/common'
-import { SlackService } from '../../vendors/slack/services/slack.service'
 import { User } from '@prisma/client'
-import { IS_PROD } from '../util/appEnvironment.util'
+import { CampaignWith } from '../../campaigns/campaigns.types'
+import { SlackService } from '../../vendors/slack/services/slack.service'
 import { SlackChannel } from '../../vendors/slack/slackService.types'
+import { IS_PROD } from '../util/appEnvironment.util'
 
 export class VoterFileDownloadAccessService {
   private readonly logger = new Logger(VoterFileDownloadAccessService.name)
-  constructor(private readonly slack: SlackService) {}
+  constructor(private readonly slack: SlackService) { }
 
   canDownload(campaign?: CampaignWith<'pathToVictory'>) {
     if (!campaign) return false
 
-    let electionTypeRequired = true
+    const details = campaign.details as { ballotLevel?: string }
+    const ballotLevel = details?.ballotLevel
+
     if (
-      campaign.details.ballotLevel &&
-      campaign.details.ballotLevel !== 'FEDERAL' &&
-      campaign.details.ballotLevel !== 'STATE'
+      ballotLevel &&
+      ballotLevel !== 'FEDERAL' &&
+      ballotLevel !== 'STATE'
     ) {
       // not required for local races
       // so we can fall back to the whole state.
-      electionTypeRequired = false
+      return true
     } else if (
-      campaign.details.ballotLevel &&
-      (campaign.details.ballotLevel === 'FEDERAL' ||
-        campaign.details.ballotLevel === 'STATE') &&
+      ballotLevel &&
+      (ballotLevel === 'FEDERAL' ||
+        ballotLevel === 'STATE') &&
       campaign.canDownloadFederal
     ) {
       // not required for federal/state races with canDownloadFederal flag
-      electionTypeRequired = false
-    }
-    if (
-      electionTypeRequired &&
-      (!campaign.pathToVictory?.data?.electionType ||
-        !campaign.pathToVictory?.data?.electionLocation)
+      return true
+    } else if (
+      campaign.pathToVictory?.data?.electionType &&
+      campaign.pathToVictory?.data?.electionLocation
     ) {
+      return true
+    } else {
       this.logger.log('Campaign is not eligible for download.', campaign.id)
       return false
-    } else {
-      return true
     }
   }
+
 
   async downloadAccessAlert(
     campaign: CampaignWith<'pathToVictory'>,
