1c3d9fcb97e069af43e337712b4ea2545dfc6d54
Swain Molster <swain.molster@gmail.com>
2026-01-24 12:22:31 -0500

    pass secrets correctly

--- packages/gp-api/deploy/utils.ts
+++
@@ -1,9 +0,0 @@
-// function to extract the username, password, and database name from the database url
-// which the docker container needs to run migrations.
-export const extractDbCredentials = (dbUrl: string) => {
-  const url = new URL(dbUrl)
-  const username = url.username
-  const password = url.password
-  const database = url.pathname.slice(1)
-  return { username, password, database }
-}
