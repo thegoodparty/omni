caefdede50e52c1c174888c54693b8f8ac83d4eb
Tomer Almog <tomer@goodparty.org>
2026-02-16 03:41:45 -0800

    feat: campaign schema

--- packages/gp-api/src/campaigns/schemas/ReadCampaignOutput.schema.ts
+++ packages/gp-api/src/campaigns/schemas/ReadCampaignOutput.schema.ts
@@ -1,31 +1,5 @@
-import { CampaignTier } from '@prisma/client'
-import { createZodDto } from 'nestjs-zod'
-import { z } from 'zod'
+import { CampaignSchema } from './Campaign.schema'
 
-export const ReadCampaignOutputSchema = z.object({
-  id: z.number(),
-  createdAt: z.coerce.date(),
-  updatedAt: z.coerce.date(),
-  slug: z.string(),
-  isActive: z.boolean(),
-  isVerified: z.boolean().nullish(),
-  isPro: z.boolean().nullish(),
-  isDemo: z.boolean(),
-  didWin: z.boolean().nullish(),
-  dateVerified: z.coerce.date().nullish(),
-  tier: z.nativeEnum(CampaignTier).nullish(),
-  formattedAddress: z.string().nullish(),
-  placeId: z.string().nullish(),
-  data: z.record(z.string(), z.unknown()),
-  details: z.record(z.string(), z.unknown()),
-  aiContent: z.record(z.string(), z.unknown()),
-  userId: z.number(),
-  canDownloadFederal: z.boolean(),
-  completedTaskIds: z.array(z.string()),
-  hasFreeTextsOffer: z.boolean(),
-  freeTextsOfferRedeemedAt: z.coerce.date().nullish(),
+export const ReadCampaignOutputSchema = CampaignSchema.omit({
+  vendorTsData: true,
 })
-
-export class ReadCampaignOutputDto extends createZodDto(
-  ReadCampaignOutputSchema,
-) {}
