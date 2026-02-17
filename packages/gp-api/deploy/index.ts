41743530859e422f749427b78dac3fb39513562d
Stephen <stephen.tanguis@gmail.com>
2026-02-17 13:02:40 -0600

    fix voter iac
    fix voter iac

--- packages/gp-api/deploy/index.ts
+++ packages/gp-api/deploy/index.ts
@@ -282,6 +282,7 @@ export = async () => {
           engine: aws.rds.EngineType.AuroraPostgresql,
           engineVersion: voterClusterLatest.engineVersion,
         })
+        voterCluster = voterClusterLatest
       }
       break
     case 'preview':
