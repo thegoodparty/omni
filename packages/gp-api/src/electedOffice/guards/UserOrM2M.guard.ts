6441130112cd6097e23b8579c69a753e872f662b
Tomer Almog <tomer@goodparty.org>
2026-02-20 12:09:49 -0500

    fix: update user type in UserOrM2MGuard to use Prisma User model

--- packages/gp-api/src/electedOffice/guards/UserOrM2M.guard.ts
+++ packages/gp-api/src/electedOffice/guards/UserOrM2M.guard.ts
@@ -1,11 +1,12 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 import { M2MToken } from '@clerk/backend'
+import { User } from '@prisma/client'
 
 @Injectable()
 export class UserOrM2MGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const { user, m2mToken } = context.switchToHttp().getRequest<{
-      user?: { id: number }
+      user?: User
       m2mToken?: M2MToken
     }>()
     return Boolean(m2mToken || user)
