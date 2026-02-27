b98fb63fb68ce4237eaac98aa05a2beb8412cade
Tomer Almog <tomer@goodparty.org>
2026-02-26 23:01:53 -0500

    chore: remove unused import export

--- packages/gp-api/src/vendors/ecanvasserIntegration/schemas/createSurvey.schema.ts
+++ packages/gp-api/src/vendors/ecanvasserIntegration/schemas/createSurvey.schema.ts
@@ -1,6 +1,4 @@
 import { CreateSurveyInputSchema } from '@goodparty_org/contracts'
 import { createZodDto } from 'nestjs-zod'
 
-export { type SurveyStatus } from '@goodparty_org/contracts'
-
 export class CreateSurveySchema extends createZodDto(CreateSurveyInputSchema) {}
