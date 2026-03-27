593028494125376b017e059a2a0f1919e0a0df05
Tomer Almog <tomer@goodparty.org>
2026-03-27 11:43:29 -0400

    chore: add missing required fields

--- packages/gp-api/src/campaigns/tasks/aiCampaignManager.types.ts
+++ packages/gp-api/src/campaigns/tasks/aiCampaignManager.types.ts
@@ -4,7 +4,9 @@ export interface StartCampaignPlanRequest {
   office_and_jurisdiction: string
   race_type: string
   number_of_opponents: number
+  seats_available: number
   win_number: number
+  incumbent_status: string
   total_likely_voters: number
   available_cell_phones: number
   available_landlines: number
