fd370f6eeba14f3d886d54ca4e74dce29bb418bd
Feliks Albright <6290668+feliksalbright@users.noreply.github.com>
2026-06-09 18:05:03 -0500

    Guard endpoint /dashboard/campaign-plan for now.

--- packages/gp-webapp/app/dashboard/campaign-plan/page.tsx
+++ packages/gp-webapp/app/dashboard/campaign-plan/page.tsx
@@ -1,6 +1,8 @@
 import pageMetaData from 'helpers/metadataHelper'
 import candidateAccess from '../shared/candidateAccess'
 import { getServerUser } from 'helpers/userServerHelper'
+import { fetchUserCampaign } from 'app/onboarding/shared/getCampaign'
+import { redirect } from 'next/navigation'
 import CampaignPlanPage from './components/CampaignPlanPage'
 
 const meta = pageMetaData({
@@ -14,6 +16,13 @@ export const dynamic = 'force-dynamic'
 
 export default async function Page(): Promise<React.JSX.Element> {
   await candidateAccess()
-  const initialUser = await getServerUser()
+  const [initialUser, campaign] = await Promise.all([
+    getServerUser(),
+    fetchUserCampaign(),
+  ])
+  // Don't show to users who haven't set up a campaign strategy during onboarding yet.
+  if (!campaign?.hasCampaignStrategy) {
+    redirect('/dashboard')
+  }
   return <CampaignPlanPage initialUser={initialUser} />
 }
