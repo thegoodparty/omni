e37b2a4bd95d8bbff9d4ac147906961947ad08eb
Kaylee <214527565+kmcost@users.noreply.github.com>
2026-06-11 11:29:08 -0500

    Update packages/gp-webapp/app/dashboard/pro-sign-up/committee-check/components/CommitteeSupportingFilesUpload.tsx
    Co-authored-by: delegate-reviewer[bot] <282107519+delegate-reviewer[bot]@users.noreply.github.com>

--- packages/gp-webapp/app/dashboard/pro-sign-up/committee-check/components/CommitteeSupportingFilesUpload.tsx
+++ packages/gp-webapp/app/dashboard/pro-sign-up/committee-check/components/CommitteeSupportingFilesUpload.tsx
@@ -171,7 +171,7 @@ export const CommitteeSupportingFilesUpload = ({
       >
         <span>Upload</span>
         {loadingFileUpload && (
-          <LoaderCircleIcon className="text-blue-300 ml-2 size-4 animate-spin" />
+          <LoaderCircleIcon className="text-info-light ml-2 size-4 animate-spin" />
         )}
       </Button>
       <HiddenFileUploadInput
