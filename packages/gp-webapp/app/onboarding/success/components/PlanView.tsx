7f834cc8786eb974e737139c8b0bb92b89775f0b
Feliks Albright <6290668+feliksalbright@users.noreply.github.com>
2026-06-11 16:31:00 -0500

    Capitalize Campaign Manager in the plan page CTA copy

--- packages/gp-webapp/app/onboarding/success/components/PlanView.tsx
+++ packages/gp-webapp/app/onboarding/success/components/PlanView.tsx
@@ -218,7 +218,7 @@ const PlanView = ({
             size="large"
             onClick={handleContinue}
           >
-            Campaign manager
+            Campaign Manager
           </Button>
         </div>
       </div>
