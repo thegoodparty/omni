8fa15671622e1be572778640bb2cad765f6eccbb
Ievgen Bazdyriev <admin@MacBook-Pro.local>
2026-06-18 12:46:42 -0700

    fix(gp-webapp): remove broken transforms from tooltip arrow

--- packages/gp-webapp/styleguide/components/ui/tooltip.tsx
+++ packages/gp-webapp/styleguide/components/ui/tooltip.tsx
@@ -136,7 +136,7 @@ function TooltipContent({
       >
         {children}
         {showArrow && (
-          <TooltipPrimitive.Arrow className="fill-components-tooltip-base z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" />
+          <TooltipPrimitive.Arrow className="fill-components-tooltip-base z-50 size-2.5" />
         )}
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
