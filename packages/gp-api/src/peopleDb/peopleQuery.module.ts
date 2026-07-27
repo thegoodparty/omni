f396cf78328b4f82da8932f731b589f6fc632676
Swain Molster <swain.molster@gmail.com>
2026-07-27 12:45:16 -0400

    feat(gp-api): port people-db csv download service

--- packages/gp-api/src/peopleDb/peopleQuery.module.ts
+++ packages/gp-api/src/peopleDb/peopleQuery.module.ts
@@ -3,6 +3,7 @@ import { DistrictService } from './services/district.service'
 import { StatsService } from './services/stats.service'
 import { VoterSampleService } from './services/voterSample.service'
 import { VoterQueryService } from './services/voterQuery.service'
+import { VoterDownloadService } from './services/voterDownload.service'
 
 @Module({
   providers: [
@@ -10,12 +11,14 @@ import { VoterQueryService } from './services/voterQuery.service'
     StatsService,
     VoterSampleService,
     VoterQueryService,
+    VoterDownloadService,
   ],
   exports: [
     DistrictService,
     StatsService,
     VoterSampleService,
     VoterQueryService,
+    VoterDownloadService,
   ],
 })
 export class PeopleQueryModule {}
