8faa85c84a6ab438f3d659515ba29af3be349d28
Swain Molster <swain.molster@gmail.com>
2026-07-27 12:36:11 -0400

    feat(gp-api): port people-db voter query service

--- packages/gp-api/src/peopleDb/peopleQuery.module.ts
+++ packages/gp-api/src/peopleDb/peopleQuery.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common'
 import { DistrictService } from './services/district.service'
 import { StatsService } from './services/stats.service'
 import { VoterSampleService } from './services/voterSample.service'
+import { VoterQueryService } from './services/voterQuery.service'
 
 @Module({
-  providers: [DistrictService, StatsService, VoterSampleService],
-  exports: [DistrictService, StatsService, VoterSampleService],
+  providers: [
+    DistrictService,
+    StatsService,
+    VoterSampleService,
+    VoterQueryService,
+  ],
+  exports: [
+    DistrictService,
+    StatsService,
+    VoterSampleService,
+    VoterQueryService,
+  ],
 })
 export class PeopleQueryModule {}
