75ac8ac11637ec1e8ed5f153d6b3c9a9d56ba77b
Swain Molster <swain.molster@gmail.com>
2026-07-31 14:14:57 -0400

    Drop unused pagination destructure left by the fenced removal

--- packages/gp-webapp/app/dashboard/contacts/[[...attr]]/components/ContactsPage.tsx
+++ packages/gp-webapp/app/dashboard/contacts/[[...attr]]/components/ContactsPage.tsx
@@ -24,7 +24,6 @@ export default function ContactsPage() {
     isCustomSegment,
     searchTerm,
     totalSegmentContacts,
-    pagination,
     isVoterDataUnavailable,
     isWinContext,
     isWinContextReady,
